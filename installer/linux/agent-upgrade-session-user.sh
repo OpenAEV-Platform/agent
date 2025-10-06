604a00ad741504478a8cefd8d7fd262752afa36d
Damien Goujard <damien.goujard@filigran.io>
2025-10-06 14:29:26 +0200

    [agent] Linux session user and service root

--- installer/linux/agent-upgrade-session-user.sh
+++ installer/linux/agent-upgrade-session-user.sh
@@ -34,6 +34,11 @@ fi
 
 echo "Starting upgrade script for ${os} | ${architecture}"
 
+# Manage the renaming OpenBAS -> OpenAEV ...
+openaev_dir=$(printf %s "${install_dir}" | sed 's/openbas/openaev/g')
+if [ -d "$openaev_dir" ]; then
+# Upgrade the agent if the folder *openaev* exists
+
 echo "01. Downloading OpenAEV Agent into ${install_dir}..."
 curl -sSfL ${base_url}/api/agent/executable/openaev/${os}/${architecture} -o ${install_dir}/openaev-agent_upgrade
 mv ${install_dir}/openaev-agent_upgrade ${install_dir}/openaev-agent
@@ -54,5 +59,22 @@ EOF
 
 echo "03. Restarting the service"
 systemctl --user restart ${session_name} || (echo "Fail restarting ${session_name}" >&2 && exit 1)
+
+else
+# Uninstall the old named agent *openbas* and install the new named agent *openaev* if the folder openaev doesn't exist
+echo "01. Installing OpenAEV Agent..."
+curl -s ${base_url}/api/agent/installer/openaev/${os}/session-user/${OPENAEV_TOKEN} | sh
+
+echo "02. Uninstalling OpenBAS Agent..."
+(
+uninstall_dir=$(printf %s "${install_dir}" | sed 's/openaev/openbas/g')
+uninstall_session=$(printf %s "${session_name}" | sed 's/openaev/openbas/g')
+rm -f ${uninstall_dir}/openbas_agent_kill.sh
+rm -f ${uninstall_dir}/openbas-agent-config.toml
+rm -f ${uninstall_dir}/openbas-agent
+systemctl --user disable ${uninstall_session} --now
+) || (echo "Error while uninstalling OpenBAS Agent" >&2 && exit 1)
+fi
+# ... Manage the renaming OpenBAS -> OpenAEV
 
 echo "OpenAEV Agent Session User started."
